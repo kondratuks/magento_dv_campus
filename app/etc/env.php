4cd4ccf37e97cb3e3450a6447fb628e01a06c122
Kondratiuk Serhii <kondratuk.s.ck@gmail.com>
2019-11-11 23:57:20 +0200

    Created new theme, added DB dump

---
+++ app/etc/env.php
@@ -0,0 +1,74 @@
+<?php
+return [
+    'backend' => [
+        'frontName' => 'admin'
+    ],
+    'crypt' => [
+        'key' => 'c16ba0b2ae23a15a20ff10a1b4c0a821'
+    ],
+    'db' => [
+        'table_prefix' => 'm2_',
+        'connection' => [
+            'default' => [
+                'host' => 'mysql',
+                'dbname' => 'serhii_kondratiuk_local',
+                'username' => 'serhii_kondratiuk_local',
+                'password' => 'serhii_kondratiuk_local',
+                'model' => 'mysql4',
+                'engine' => 'innodb',
+                'initStatements' => 'SET NAMES utf8;',
+                'active' => '1'
+            ]
+        ]
+    ],
+    'resource' => [
+        'default_setup' => [
+            'connection' => 'default'
+        ]
+    ],
+    'x-frame-options' => 'SAMEORIGIN',
+    'MAGE_MODE' => 'developer',
+    'session' => [
+        'save' => 'files'
+    ],
+    'cache' => [
+        'frontend' => [
+            'default' => [
+                'id_prefix' => '40d_'
+            ],
+            'page_cache' => [
+                'id_prefix' => '40d_'
+            ]
+        ]
+    ],
+    'lock' => [
+        'provider' => 'db',
+        'config' => [
+            'prefix' => null
+        ]
+    ],
+    'cache_types' => [
+        'config' => 1,
+        'layout' => 1,
+        'block_html' => 0,
+        'collections' => 1,
+        'reflection' => 1,
+        'db_ddl' => 1,
+        'compiled_config' => 1,
+        'eav' => 1,
+        'customer_notification' => 1,
+        'config_integration' => 1,
+        'config_integration_api' => 1,
+        'full_page' => 0,
+        'config_webservice' => 1,
+        'translate' => 1,
+        'vertex' => 1
+    ],
+    'install' => [
+        'date' => 'Mon, 07 Oct 2019 15:43:20 +0000'
+    ],
+    'downloadable_domains' => [
+        'serhii-kondratiuk.local',
+        'serhii-kondratiuk-2.local'
+    ]
+];
---
+++ dev/tools/grunt/configs/local-themes.js
@@ -0,0 +1,32 @@
+module.exports = {
+    serhiik_luma_en_US: {
+        area: 'frontend',
+        name: 'Serhiik/luma',
+        locale: 'en_US',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    serhiik_luma_uk_UA: {
+        area: 'frontend',
+        name: 'Serhiik/luma',
+        locale: 'uk_UA',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    },
+    serhiik_luma_ru_RU: {
+        area: 'frontend',
+        name: 'Serhiik/luma',
+        locale: 'ru_RU',
+        files: [
+            'css/styles-m',
+            'css/styles-l'
+        ],
+        dsl: 'less'
+    }
+};
